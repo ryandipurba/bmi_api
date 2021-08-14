7110c9b7df6cf47d8cb77299eb85e32c297c5cbd
Ryandi Purba <purbaryandi@gmail.com>
2021-08-14 10:08:17 +0700

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json());
 
 Countbmi = (weight, height) => {
